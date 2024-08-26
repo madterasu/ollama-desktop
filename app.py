89e61dd5a53a3db982617005dfbdbb193517135e
Achmad Maulana Rochman <achmadmaulanarochman@gmail.com>
2024-08-27 01:55:41 +0700

    update app.py

--- app.py
+++ app.py
@@ -8,3 +8,18 @@ import markdown
 import httpx
 from pyollama import run_model, convert_nanoseconds_to_seconds, list_models_names
 
+class AppWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.window_width, self.window_height = 800, 800
+        self.setMinimumSize(self.window_width, self.window_height)
+        self.setWindowTitle('Ollama Prompt & Response App V1')
+        self.setWindowIcon(QIcon('./icons/logo.png'))
+        self.setObjectName('mainwindow')
+        # Qwidget:window {background-color: #1b2b39;}
+        self.setStyleSheet('''
+            font-size: 16px;
+        ''')
+        self.layout = {}
+        self.layout['main'] = QVBoxLayout()
+        self.setLayout(self.layout['main'])
